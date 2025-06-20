55b3db1ca7ee7c8ae613b9969c776554c2a0c5b1
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-21 01:52:38 +0600

    borrow a book summary

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -8,10 +8,10 @@ export const borrowRoute = express.Router()
 
 borrowRoute.post('/borrow-book', async (req: Request, res: Response) => {
     try {
-        const {book, quantity, dueDate} = req.body;
+        const { book, quantity, dueDate } = req.body;
         const updateBook = await Books.borrowBook(book, quantity);
 
-        const borrow = await Borrow.create({book, quantity, dueDate})
+        const borrow = await Borrow.create({ book, quantity, dueDate })
 
 
         res.status(201).json({
@@ -20,7 +20,54 @@ borrowRoute.post('/borrow-book', async (req: Request, res: Response) => {
             data: borrow
         })
 
-    } catch (error:any) {
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error
+        });
+    }
+})
+
+
+
+borrowRoute.get('/summary', async (req: Request, res: Response) => {
+    try {
+        const summary = await Borrow.aggregate([
+            {
+                $group: {
+                    _id: "$book",
+                    totalQuantity: { $sum: "$quantity" }
+                }
+            },
+            {
+                $lookup: {
+                    from: "books",
+                    localField: "_id",
+                    foreignField: "_id",
+                    as: "bookInfo"
+                }
+            },
+            {
+                $unwind: "$bookInfo"
+            },
+            {
+                $project: {
+                    book: {
+                        title: "$bookInfo.title",
+                        isbn: "$bookInfo.isbn"
+                    },
+                    totalQuantity: 1
+                }
+            }
+        ])
+
+        res.status(200).json({
+            success: true,
+            message: "Borrowed books summary retrieved successfully",
+            data: summary
+        });
+    } catch (error: any) {
         res.status(500).json({
             success: false,
             message: error.message,
