baf8330ea44f4af843bf1e3d4d7d993a63cee3b0
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-20 23:27:20 +0600

    update and delete route

--- src/app/controllers/books.controller.ts
+++ src/app/controllers/books.controller.ts
@@ -3,13 +3,112 @@ import { Books } from '../models/books.model';
 
 export const booksRoute = express.Router();
 
-booksRoute.post('/create-book', async (req: Request, res: Response) =>{
+
+// create book
+booksRoute.post('/create-book', async (req: Request, res: Response) => {
     const body = req.body;
     const book = await Books.create(body);
 
     res.status(201).json({
         success: true,
-        message: 'book created successfully',
+        message: 'Book created successfully',
         data: book
     })
+})
+
+
+// Get all books with support filtering and sorting
+booksRoute.get('/', async (req: Request, res: Response) => {
+    try {
+        const filter = (req.query.filter as string) || undefined;
+        const sortBy = (req.query.sortBy as string) || "createdAt";
+        const sort = (req.query.sort as string) || "desc";
+        const limit = Number(req.query.limit) || 5;
+
+
+        const genreFilter = filter ? { genre: filter } : {};
+        const sorting = { [sortBy]: sort === "desc" ? -1 : 1 } as any;
+
+
+        const books = await Books.find(genreFilter).sort(sorting).limit(limit);
+
+
+        res.status(200).json({
+            success: true,
+            message: "Books retrieved successfully",
+            data: books
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong",
+            error
+        });
+    }
+});
+
+
+// get a single book
+booksRoute.get('/:bookId', async (req: Request, res: Response) => {
+    try {
+        const bookId = req.params.bookId;
+        const book = await Books.findById(bookId);
+
+        res.status(200).json({
+            success: true,
+            message: "Book retrieved successfully",
+            data: book
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong",
+            error
+        });
+    }
+})
+
+
+// update a book
+booksRoute.put('/:bookId', async (req: Request, res: Response) => {
+    try {
+        const bookId = req.params.bookId;
+        const updatedBody = req.body;
+        const book = await Books.findByIdAndUpdate(bookId, updatedBody, { new: true })
+
+        res.status(200).json({
+            success: true,
+            message: "Book updated successfully",
+            data: book
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong",
+            error
+        });
+    }
+})
+
+
+// delete a book
+booksRoute.delete('/:bookId', async (req: Request, res: Response) => {
+    try {
+        const bookId = req.params.bookId;
+
+        await Books.findByIdAndDelete(bookId);
+
+        res.status(200).json({
+            success: true,
+            message: "Book deleted successfully",
+            data: null
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong",
+            error
+        });
+    }
 })
